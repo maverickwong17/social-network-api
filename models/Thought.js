28f6b61dd61585a9aa438b6f7c9e0a5b769bebd1
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-08-17 22:00:02 -0700

    getters for thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,11 +1,16 @@
 const { Schema, model } = require('mongoose')
+const moment = require('moment')
+
+const formatTime = (timestamp) => {
+    return moment(timestamp).format("MMM Do ,YYYY [at] h:mm")
+}
 
 const reactionSchema = new Schema(
     {
         reactionId: { type: Schema.Types.ObjectId, default: new Schema.Types.ObjectId},
         reactionBody: { type: String, required: true, maxLength: 280},
         username: { type: String, required: true,},
-        createdAt: { type: Date, default: Date.now, } // getter method to format
+        createdAt: { type: Date, default: Date.now, get: formatTime, } // getter method to format
     },
     {
         toJSON: {virtuals: true},
@@ -17,12 +22,12 @@ const reactionSchema = new Schema(
 const thoughtSchema = new Schema(
     {
         thoughtText : { type: String, required: true, minLength: 1, maxLength: 280, },
-        createdAt: { type: Date, default: Date.now }, // getter method to format
+        createdAt: { type: Date, default: Date.now, get: formatTime }, // getter method to format
         username: { type: String, required: true },
         reaction: [reactionSchema],
     },
     {
-        toJSON: {virtuals: true},
+        toJSON: { virtuals: true ,  getters: true },
         id: false
     }
 )
@@ -30,6 +35,7 @@ const thoughtSchema = new Schema(
 
 // reactionSchema.get(function(v){})
 // thoughtSchema.get(function(v){})
+
 
 thoughtSchema.virtual('reactionCount').get(function(v){
     return this.reaction.length
